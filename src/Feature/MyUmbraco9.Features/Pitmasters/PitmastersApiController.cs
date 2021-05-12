f8384d62608cc13afcbe02f4c12d5319b821076f
Ali <aegraham82@hotmail.com>
2021-05-12 20:44:28 +0100

    Added a infrastructure project and set models builder to build there and also added a features project

---
+++ src/Feature/MyUmbraco9.Features/Pitmasters/PitmastersApiController.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="twentysix" file="PitmastersApiController.cs">
+// Copyright (c) twentysix.  All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MyUmbraco9.Features.Pitmasters
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using MyUmbraco9.Infrastructure.DocumentTypes.GeneratedModels;
+
+    using Umbraco.Cms.Core;
+    using Umbraco.Cms.Web.Common.Controllers;
+
+    public class PitmastersApiController : UmbracoApiController
+    {
+        private readonly IPublishedContentQuery _contentQuery;
+        public PitmastersApiController(IPublishedContentQuery contentQuery)
+        {
+            _contentQuery = contentQuery;
+        }
+
+ 
+        [HttpGet]
+        public string GetPitmasters()
+        {
+            var pitmasters = _contentQuery.ContentAtRoot().OfType<PitMasters>().FirstOrDefault();
+            var returnValue = string.Empty;
+            foreach (Pitmaster child in pitmasters.Children)
+            {
+                returnValue += child.FullName;
+            }
+            
+            return returnValue;
+        }
+    }
+}
